026b3736bca50c72b2751fa27a78aa2f5e1e7a9b
sai8237 <74070458+sai8237@users.noreply.github.com>
2020-11-17 12:06:27 +0530

    Update modal.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { motion } from 'framer-motion';
 
-export const Modal = ({ setSelectedImg, selectedImg }) => {
-
+export const Modal = ({ images, setSelectedImg, selectedImg, index }) => {
+  const [idex, setIndex] = useState(index);
+  console.log(idex);
   const handleClick = (e) => {
     if (e.target.classList.contains('backdrop')) {
-      setSelectedImg(null);
+      setSelectedImg(false);
     }
+  }
+  const nextImage = (e) => {
+    if(idex != images.length-1)
+    setIndex(idex+1)
+  }
+  const prevImage = (e) => {
+    if(idex != 0){
+      setIndex(idex-1)
+    }  
   }
 
   return (
@@ -14,7 +24,9 @@ export const Modal = ({ setSelectedImg, selectedImg }) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
-      <motion.img src={selectedImg} alt="enlarged pic" 
+      <motion.button className="left" onClick = {prevImage}>prev</motion.button>
+      <motion.button className="right" onClick = {nextImage}>next</motion.button>
+      <motion.img src={images[idex].urls.thumb} alt="enlarged pic" 
         initial={{ y: "-100vh" }}
         animate={{ y: '20vh' }}
       />
